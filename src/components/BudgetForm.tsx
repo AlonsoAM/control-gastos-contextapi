bd483f72825a45441d70eaaaa46f1be3786217d2
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-27 18:35:16 -0500

    feat: implementar formulario de presupuesto con manejo de estado

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,42 @@
-const BudgetForm = () => {
-  return (
-    <div>BudgetForm</div>
-  )
-}
+import { useState } from "react";
 
-export default BudgetForm
+const BudgetForm = () => {
+  const [budget, setBudget] = useState(0);
+
+  const handleBudgetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(Number(e.target.value));
+  };
+
+  return (
+    <>
+      <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+          <label
+            htmlFor="budget"
+            className="text-4xl text-blue-600 font-bold text-center"
+          >
+            Definir Presupuesto
+          </label>
+          <input
+            id="budget"
+            type="number"
+            className="w-full bg-white border border-gray-200 p-2"
+            placeholder="Define tu Presupuesto"
+            name="budget"
+            value={budget}
+            onChange={handleBudgetChange}
+            min={0}
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Definir Presupuesto"
+          className="w-full bg-blue-600 hover:bg-blue-700 cursor-pointer p-2 text-white font-black uppercase"
+        />
+      </form>
+    </>
+  );
+};
+
+export default BudgetForm;
