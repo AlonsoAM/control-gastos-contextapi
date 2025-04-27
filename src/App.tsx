5cb8ae9eb31147e68619fd77173e83a049c65c86
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-27 18:19:10 -0500

    Add new SVG icons for various categories including home, food, expenses, leisure, health, and subscriptions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1 className='text-blue-500 text-3xl'>Hola Mundo</h1>
+      <h1 className='text-blue-500 text-3xl'>Control de Gastos</h1>
     </>
   )
 }
