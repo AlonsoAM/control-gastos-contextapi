d829b9f058b754ce09a560bafa4db9c6f5460a3e
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-27 18:44:56 -0500

    feat: agregar reducer para manejar el estado del presupuesto

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,24 @@
+export type BudgetActions = {
+  type: "ADD_BUDGET";
+  payload: { budget: number };
+};
+
+export type BudgetState = {
+  budget: number;
+};
+
+export const initialBudgetState: BudgetState = {
+  budget: 0,
+};
+
+export const budgetReducer = (
+  state: BudgetState = initialBudgetState,
+  action: BudgetActions
+): BudgetState => {
+  switch (action.type) {
+    case "ADD_BUDGET":
+      return { ...state, budget: action.payload.budget };
+    default:
+      return state;
+  }
+};
