265b7bae9f307b0fa7bdeeda613e449aac9c9909
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-27 19:01:53 -0500

    feat: agregar contexto y proveedor para manejar el estado del presupuesto

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,26 @@
+import { Dispatch, useReducer } from "react";
+import {
+  BudgetActions,
+  budgetReducer,
+  BudgetState,
+  initialBudgetState,
+} from "../reducers/budget-reducer";
+
+import { createContext } from "react";
+
+export type BudgetContextType = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+};
+
+export const BudgetContext = createContext<BudgetContextType>(null!);
+
+export const BudgetProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialBudgetState);
+
+  return (
+    <BudgetContext.Provider value={{ state, dispatch }}>
+      {children}
+    </BudgetContext.Provider>
+  );
+};
