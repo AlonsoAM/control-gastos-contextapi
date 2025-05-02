b136f68e4fb97a6fe0fa838b21c4770d5746c5dd
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-02 13:35:53 -0500

    feat: agregar formulario de gastos con manejo de estado y selección de fecha

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,6 +1,19 @@
 import {categories} from "../data/categories.ts";
+import DatePicker from "react-date-picker";
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import {useState} from "react";
+import {DraftExpense} from "../types";
 
 const ExpenseForm = () => {
+
+    const [expense, setExpense] = useState<DraftExpense>({
+        expenseName: '',
+        amount: 0,
+        category: '',
+        date: new Date()
+    })
+
     return (
         <>
             <form className={'space-y-5'}>
@@ -16,6 +29,12 @@ const ExpenseForm = () => {
                            placeholder={'Agrega el nombre de un gasto'}
                            className={'bg-slate-100 p-2 '}
                            name={'expenseName'}
+                           value={expense.expenseName}
+                           onChange={(e) => setExpense({
+                               ...expense,
+                               expenseName: e.target.value
+                           })} // Update the state with the new value
+
                     />
                 </div>
                 <div className={'flex flex-col gap-2'}>
@@ -27,6 +46,11 @@ const ExpenseForm = () => {
                            placeholder={'Agrega la cantidad del gasto. Ej. 300'}
                            className={'bg-slate-100 p-2 '}
                            name={'amount'}
+                           value={expense.amount}
+                           onChange={(e) => setExpense({
+                               ...expense,
+                               amount: Number(e.target.value)
+                           })} // Update the state with the new value
                     />
                 </div>
                 <div className={'flex flex-col gap-2'}>
@@ -36,6 +60,11 @@ const ExpenseForm = () => {
                     <select id={'category'}
                             className={'bg-slate-100 p-2 '}
                             name={'category'}
+                            value={expense.category}
+                            onChange={(e) => setExpense({
+                                ...expense,
+                                category: e.target.value
+                            })} // Update the state with the new value
                     >
                         <option value="">Seleccione</option>
                         {categories.map(category => (
@@ -44,6 +73,20 @@ const ExpenseForm = () => {
                             </option>
                         ))}
                     </select>
+
+                </div>
+                <div className={'flex flex-col gap-2'}>
+                    <label className={'text-xl'} htmlFor='amount'>
+                        Fecha Gasto:
+                    </label>
+                    <DatePicker
+                        className={'bg-slate-100 p-2 border-0'}
+                        value={expense.date}
+                        onChange={(date) => setExpense({
+                            ...expense,
+                            date: date
+                        })} // Update the state with the new value
+                    />
                 </div>
                 <input type="submit"
                        className={'bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg'}
